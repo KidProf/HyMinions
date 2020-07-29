395f2616180d6f0d4f1495159a9ab82422fa5c6a
KidProf <bitballoon2018@gmail.com>
2020-07-29 14:30:01 +0800

    changed port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const getProfileApi = require('./routes/api/getMinionsApi');
 
 //constants
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //ROUTING
 //views
