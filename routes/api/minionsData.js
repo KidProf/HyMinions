d0b3136e7c05702e68faa6252d2b01a6fa04b4b0
KidProf <bitballoon2018@gmail.com>
2021-09-14 17:08:38 +0800

    fixed clay minion receipe, cleaned home page

--- routes/api/minionsData.js
+++ routes/api/minionsData.js
@@ -2524,7 +2524,7 @@ exports.minions = [
         storage: [64,192,192,384,384,576,576,768,768,960,960],
         upgrade:{
             materials: [
-                ["Wooden Shovel","Clay"],
+                ["Wooden Pickaxe","Clay"],
                 ["Clay"],
                 ["Clay"],
                 ["Clay"],
